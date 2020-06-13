bc3faf3102354332163a6dda8e68fcf8fe8378ea
Ahmad Hussein <ahmadhussein99@yahoo.com>
2020-06-13 20:43:59 +0300

    Sticky Menu

--- dist/main.js
+++ dist/main.js
@@ -6,3 +6,16 @@ function navToggle() {
     nav.classList.toggle('flex');
     nav.classList.toggle('hidden');
 }
+
+var nav = document.getElementById('site-menu');
+var header = document.getElementById('top');
+
+window.addEventListener('scroll', function() {
+    if (window.scrollY >=400) { // adjust this value based on site structure and header image height
+        nav.classList.add('nav-sticky');
+        header.classList.add('pt-scroll');
+    } else {
+        nav.classList.remove('nav-sticky');
+        header.classList.remove('pt-scroll');
+    }
+});
